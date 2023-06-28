e04a345607e510ab38b9ebf5e4668b61d5918704
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-06-28 16:10:05 -0300

    02:09 - Criando a camada de model e controller

---
+++ src/controller/todo-controller.js
@@ -0,0 +1,19 @@
+import { TodoListModel, TarefaModel } from '../model/todo-model.js';
+import { TodoListView, } from '../view/todo-view.js'
+
+export class TodoListController {
+    view = new TodoListView();
+    todoListModel = new TodoListModel();
+    constructor(todoListModel, view) {
+        this.todoListModel = todoListModel;
+        this.view = view;
+    }
+
+
+    async addItem() {
+        const { title, dueDate } = this.view.displayAddItem();
+        const tarefa = new TarefaModel(title, dueDate);
+        this.todoListModel.add(tarefa)
+        this.view.log(`\nTarefa "${title}" adicionado\n`);
+    }
+}
---
+++ src/model/todo-model.js
@@ -0,0 +1,52 @@
+export class TarefaModel {
+    #title;
+    #dueDate;
+    #status;
+    #id;
+
+    constructor(title, dueDate) {
+        this.#title = title;
+        this.#dueDate = dueDate;
+        this.#status = 'pending';
+        this.#id = Date.now();
+    }
+
+
+    get title() {
+        return this.#title;
+    }
+
+    set title(value) {
+        this.#title = value;
+    }
+
+    get dueDate() {
+        return this.#dueDate;
+    }
+
+    set dueDate(value) {
+        this.#dueDate = value;
+    }
+
+    get status() {
+        return this.#status;
+    }
+
+    set status(value) {
+        this.#status = value;
+    }
+
+    get id() {
+        return this.#id;
+    }
+}
+
+
+export class TodoListModel {
+    todoList = [];
+    constructor() { }
+
+    add(tarefa) {
+        this.todoList.push(tarefa);
+    }
+}
