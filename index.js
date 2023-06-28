d133e1ef0a584e2bf34311b37ae67c9257a38b28
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-06-28 12:02:15 -0300

    01:06 - Editando Tarefa

--- index.js
+++ index.js
@@ -48,6 +48,45 @@ async function completarTarefa() {
 
 }
 
+
+async function editarTarefa() {
+    if (!todoList.length) {
+        console.log(`\nNenhuma tarefa encontrada\n`)
+        return;
+    }
+
+    const { id } = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Qual tarefa você quer editar?',
+            choices: todoList.map((tarefa) => ({ name: tarefa.title, value: tarefa.id })),
+        }
+    ]);
+
+    const tarefa = todoList.find((tarefa) => tarefa.id === id);
+
+    const { title, dueDate } = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Digite um novo titulo',
+            default: tarefa.title,
+        },
+        {
+            type: 'input',
+            name: 'dueDate',
+            message: 'Digite uma nova data da tarefa',
+            default: tarefa.dueDate,
+        },
+    ])
+
+    tarefa.title = title;
+    tarefa.dueDate = dueDate;
+    console.log(`\nTarefa "${title}" editada com sucesso\n`);
+
+}
+
 async function main() {
     do {
 
@@ -60,6 +99,7 @@ async function main() {
                     'Adicionar tarefa',
                     'Listar tarefas',
                     'Marcar tarefa como concluído',
+                    'Editar tarefa',
                     "Sair"
                 ]
             }
@@ -75,6 +115,9 @@ async function main() {
                 break;
             case 'Marcar tarefa como concluído':
                 await completarTarefa();
+                break;
+            case 'Editar tarefa':
+                await editarTarefa();
                 break;
             case 'Sair':
                 return;
